eecba1768c0537324d84ed631d7d20a64ee18486
jpc19832020-tech <jpc19832020@gmail.com>
2025-09-29 14:13:33 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function iframeError() {
+    var iframe = document.getElementById('foton-iframe');
+    iframe.style.display = 'none';
+    document.getElementById('embed-block-message').style.display = 'block';
+}
